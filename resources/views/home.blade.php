c5839108551909022ae86e5b12d00b922726b91f
RishikeshKumarSingh <rishikeshsingh2807@gmail.com>
2022-05-26 10:20:54 +0530

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Student management system</title>
+        <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" 
+integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+                <div class="container">
+                        <a href="" class="navbar-brand">SMS</a>
+                </div>
+        </nav>
+
+        <div class="container mt-4">
+                <div class="row">
+                        <div class="col-3">
+                                <div class="card">
+                                        <div class="card-body">
+                                                <h6>Insert Data</h6>
+                                                <form action="" method="post">
+                                                        <div class="mb-3">
+                                                                <label for="">Name</label>
+                                                                <input type="text" class="form-control">
+                                                        </div>
+                                                        <div class="mb-3">
+                                                                <label for="">Contact</label>
+                                                                <input type="text" class="form-control">
+                                                        </div>
+                                                        <div class="mb-3">
+                                                                <label for="">Email</label>
+                                                                <input type="text" class="form-control">
+                                                        </div>
+                                                        <div class="mb-3">
+                                                                <input type="submit" class="btn btn-success w-100">
+                                                        </div>
+                                                </form>
+                                        </div>
+                                </div>
+                        </div>
+                        <div class="col-9"></div>
+                </div>
+        </div>
+</body>
+</html>
